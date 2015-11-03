ef79abc780fd7f04c8d8fe46a0cfdbebe61a3929
alu0100207231 <alu0100207231@ull.edu.es>
2015-11-03 17:05:13 +0000

    Expectativas para la creación de un libro en la bibliografía

--- spec/bibliografia_spec.rb
+++ spec/bibliografia_spec.rb
@@ -1,11 +1,44 @@
 require 'spec_helper'
+require 'lib/bibliografia'
 
 describe Bibliografia do
-  it 'has a version number' do
-    expect(Bibliografia::VERSION).not_to be nil
-  end
+   before :each do
+      @libro1 = Bibliografia.new(["Dave Thomas", "Andy Hunt", "Chad Fowler"], "Programming Ruby 1.9 & 2.0: The Pragmatic Programmers' Guide", "(The Facets of Ruby)", "Pragmatic Bookshelf", "4 edition", "(July 7, 2013)", ["ISBN-13: 978-1937785499", "ISBN-10: 1937785491"])
+   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
+   it 'has a version number' do
+      expect(Bibliografia::VERSION).not_to be nil
+   end
+
+   it 'does something useful' do
+      expect(false).to eq(true)
+   end
+
+   it 'Debe existir uno o más autores' do
+      expect(@libro1.autores).not_to be_empty
+   end
+
+   it 'Debe existir un título' do
+      expect(@libro1.título).not_to be_empty
+   end
+
+   it 'Debe existir o no una serie' do
+
+   end
+
+   it 'Debe existir una editorial' do
+      expect(@libro1.editorial).not_to be_empty
+   end
+
+   it 'Debe existir un número de edición' do
+      expect(@libro1.edición).not_to be_empty
+   end
+
+   it 'Debe existir una fecha de publicación' do
+      expect(@libro1.fecha).not_to be_empty
+   end
+
+   it 'Debe existir uno o más números ISBN' do
+      
+   end
 end
