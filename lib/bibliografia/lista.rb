8442fb0540e798c9db93d60bfa6449c8da7413ea
belfor <alemandeleon86@gmail.com>
2015-11-09 11:57:43 +0000

    Añadida expectativa insertar mas de uno y realizada las funciones unshift y push, expectativa superada

--- lib/bibliografia/lista.rb
+++ lib/bibliografia/lista.rb
@@ -3,7 +3,19 @@ Nodo = Struct.new(:dato, :sig) do
     
     def initialize(dato, sig)
         @dato = dato
-        @sgi = sig
+        @sig = sig
+    end
+    
+    def getSig
+        return @sig
+    end
+    
+    def getDato
+        return @dato
+    end
+    
+    def next_(sig)
+        @sig = sig
     end
     
 end
@@ -12,6 +24,42 @@ class Lista
     attr_accessor :head
     
     def initialize()
-        @head = null;
+        @head = nil;
     end
+    
+    def unshift(dato)
+        primero = @head
+        @head = Nodo.new(dato,primero)
+    end
+    def push(dato)
+        last = @head
+        if last!= nil
+            while last.getSig != nil
+                last = last.getSig
+            end
+            last.next_(Nodo.new(dato,nil))
+        else
+            @head = Nodo.new(dato,nil)
+        end
+    end
+    
+    def length
+        if @head == nil
+            num = 0
+        else
+            last = @head
+            num = 1
+            while last.getSig != nil
+                last = last.getSig
+                num += 1
+            end
+        end
+        num
+    end
+            
 end
+
+l = Lista.new
+l.push(2)
+l.push(3)
+puts l.length
--- spec/bibliografia_spec.rb
+++ spec/bibliografia_spec.rb
@@ -79,6 +79,21 @@ describe Bibliografia do
     context Lista do
         before :each do
             @refs = Lista.new
+            @libro1 = Referencia.new(["Dave Thomas", "Andy Hunt", "Chad Fowler"], "Programming Ruby 1.9 & 2.0: The Pragmatic Programmers' Guide", "(The Facets of Ruby)", "Pragmatic Bookshelf", "4 edition", "(July 7, 2013)", ["ISBN-13: 978-1937785499", "ISBN-10: 1937785491"])
+            @libro2 = Referencia.new("Scott Chacon", "Pro Git 2009th Edition", "(Pro)", "Apress", "2009 edition", "(August 27, 2009)", ["ISBN-13: 978-1430218333", "ISBN-10: 1430218339"])
+            @libro3 = Referencia.new(["Dave Flanagan", "Yukihiro Matsumoto", "Chad Fowler"], "The Ruby Programming Language", "(Pro)", "O’Reilly Media", "1 edition", "(February 4, 2008)", ["ISBN-10: 0596516177", "ISBN-13: 978-0596516178"])
+            @libro4 = Referencia.new(["Dave Chelimsky,", "Dave Astels", "Bryan Helmkamp","Dan North","zach Dennis","Aslak Hellesoy"], "The RSpec Book: Behaviour Driven Development with RSpec, Cucumber, and Friends", "(The Facets of Ruby)", "Pragmatic Bookshelf", "1 edition", "(February 4, 2008)", ["ISBN-10: 1934356379", "ISBN-13: 978-1934356371"])
+            @libro5 = Referencia.new("Richard E", "Git Pocket Guide O’Reilly Media", "(The Facets of Ruby)", "Pragmatic Bookshelf", "1 edition", "(February 4, 2008)", ["ISBN-10: 1934356379", "ISBN-13: 978-1934356371"])
+        end
+        
+        it 'Se puede insertar varios elementos' do
+            n = @refs.length
+            @refs.push(@libro1)
+            @refs.push(@libro2)
+            @refs.push(@libro3)
+            @refs.push(@libro4)
+            @refs.push(@libro5)
+            expect(@refs.length == 0)
         end
     end
 end
